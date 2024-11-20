b89a8f8db103904c3bc8464befd37393a1720890
paolopreve <paoloprevedelloramirez@gmail.com>
2024-11-20 11:02:25 +0100

    created class time

---
+++ FirstProject/FirstProject/Time.cpp
@@ -0,0 +1,10 @@
+#include "Time.h"
+
+Time::Time(int sec)
+{
+	this->sec = sec;
+}
+int Time::GetSec()
+{
+	return sec;
+}
---
+++ FirstProject/FirstProject/Time.h
@@ -0,0 +1,10 @@
+#pragma once
+class Time
+{
+private:
+	int sec;
+public:
+	Time(int sec = 0);
+	int GetSec();
+};
+
